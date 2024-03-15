342cfb06321ae19936bd335bbf0256a1f214fee4
Graham Stein <39591264+Graham-Stein@users.noreply.github.com>
2024-03-15 07:24:53 +0000

    Update aliases.zsh

--- aliases.zsh
+++ aliases.zsh
@@ -7,6 +7,9 @@ fzf_aws_prof() {
 }
 alias ap="fzf_aws_prof"
 
+# History with fuzzy search, reverse order, not sorted
+alias hg="history | fzf +s --tac"
+
 # Get Parameter from AWS Param store
 get_param () {
   echo "AWS PROFILE:"
@@ -16,3 +19,17 @@ get_param () {
   echo $(aws ssm get-parameter --name=$1 | jq -r '.Parameter.Value')
 }
 alias gp="get_param"
+
+# git
+alias co="git checkout"
+
+# general
+alias ls="ls -lah"
+alias who="echo $(whoami)"
+alias ez="exec zsh"
+
+# Docker
+alias d="docker"
+alias dc="docker compose"
+alias dsp="docker system prune"
+
